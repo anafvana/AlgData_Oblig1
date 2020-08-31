a8d12f6a89f8d103cea7c872c9c23bacc1a6983e
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2020-08-31 12:45:19 +0200

    Oppgave 10 - Øyvind

--- src/main/Oblig1.java
+++ src/main/Oblig1.java
@@ -10,15 +10,15 @@ public class Oblig1 {
 
     //TODO: Han sier at vi bør unngå norsketegn i kommentarer. Skal vi bytte språk til engelsk?
 
-    //Oppgave 1 - finne største tall
-    // Spørsmål etter oppgave 1:
-    // Spm: Når blir det flest ombyttinger?
-    // Svar: Det blir flest ombyttinger når det største tallet er først.
-    // Spm: Når blir det færrest?
-    // Svar: Når arrayet er sortert stiggende.
+    //Oppgave 1 - finne stoerste tall
+    // Spoersmål etter oppgave 1:
+    // Spm: Naar blir det flest ombyttinger?
+    // Svar: Det blir flest ombyttinger når det stoerste tallet er foerst.
+    // Spm: Når blir det faerrest?
+    // Svar: Når arrayet er sortert stigende.
     // Spm: Hvor mange blir det i gjennomsnitt?
-    // Svar: Det blir i gjennomsnitt 1/2 + 1/3 + 1/4 + 1/5 + ... + 1/n (Hn - 1) antall ganger vi går inn i if setningen. Denne rekken er Hn som er nesten det samme som log(n) + 0.577.
-    //       Hvis n i vårt eksempel er 1 000 000 000 så går vi inn i if setningen i gjennomsnitt log(1000000000) + 0.577 ganger, som er 9.577.
+    // Svar: Det blir i gjennomsnitt 1/2 + 1/3 + 1/4 + 1/5 + ... + 1/n (Hn - 1) antall ganger vi gaar inn i if setningen. Denne rekken er Hn som er nesten det samme som log(n) + 0.577.
+    //       Hvis n i vaart eksempel er 1 000 000 000 så går vi inn i if setningen i gjennomsnitt log(1000000000) + 0.577 ganger, som er 9.577.
     public static int maks(int[] a) {
         sjekkArray(a);
         int storste;
@@ -27,8 +27,8 @@ public class Oblig1 {
             storste = a[i - 1];
             neste = a[i];
             if (storste > neste) {
-                // Dersom storste er større enn neste tallet bytter de plass
-                // Slik at det største tallet kommer på siste plass
+                // Dersom stoerste er større enn det neste tallet bytter de plass
+                // Slik at det stoerste tallet kommer på siste plass
                 a[i] = storste;
                 a[i - 1] = neste;
             }
@@ -38,16 +38,16 @@ public class Oblig1 {
 
     //Oppgave 1 - Telle antall ombyttinger
 
-    // Spørsmål til ombyttingsoppgaven:
-    // Lag tilfeldige permutasjoner av tallene fra 1 til n og bruk så metoden.
-    // På den måten kan du få en indikasjon på hvor mange det blir i gjennomsnitt (det finnes en formel for gjennomsnittet).
-    // Kan du på grunnlag av dette si om metoden maks er bedre (eller dårligere) enn de maks-metodene vi har sett på tidligere?
+    // Spoersmål til ombyttingsoppgaven:
+    // Lag tilfeldige permutasjoner av tallene fra 1 til n og bruk saa metoden.
+    // Paa den maaten kan du få en indikasjon på hvor mange det blir i gjennomsnitt (det finnes en formel for gjennomsnittet).
+    // Kan du paa grunnlag av dette si om metoden maks er bedre (eller daarligere) enn de maks-metodene vi har sett på tidligere?
 
-    // Svar: Denne maks-metoden vil være dårligere. Vi looper gjennom for-løkken 2n ganger, men for if testen vil vi gå inn her
-    // 4(n-1) ganger. Noe som gjør hele metoden mye tregere enn de forrige maks metodene. Med tilfeldige permutasjoner
-    // av tall fra f.eks 1-100 ble resultatet nesten alltid 90+ ombyttinger. Dette viser også at metoden er mye tregere.
-    // Vi la til maks-metoden i Program klassen fra pensum, og det tok rundt 270 millisek å utføre den.
-    // De tre andre maks-metodene vi har sett på i pensum tok fra 85-40 millisek. Så denne metoden er definitivt betydelig tregere.
+    // Svar: Denne maks-metoden vil være daarligere. Vi looper gjennom for-loekken 2n ganger, men for if testen vil vi gå inn her
+    // 4(n-1) ganger. Noe som gjoer hele metoden mye tregere enn de forrige maks metodene. Med tilfeldige permutasjoner
+    // av tall fra f.eks 1-100 ble resultatet nesten alltid 90+ ombyttinger. Dette viser ogsaa at metoden er mye tregere.
+    // Vi la til maks-metoden i Program klassen fra pensum, og det tok rundt 270 millisek aa utføre den.
+    // De tre andre maks-metodene vi har sett på i pensum tok fra 85-40 millisek. Saa denne metoden er definitivt betydelig tregere.
 
     public static int ombyttinger(int[] a) {
         sjekkArray(a);
@@ -67,7 +67,7 @@ public class Oblig1 {
     }
 
     public static void sjekkArray(int[] a) {
-        if (a.length == 0) throw new NoSuchElementException("Arrayet er tom!");
+        if (a.length == 0) throw new NoSuchElementException("Arrayet er tomt!");
     }
 
     //Oppgave 2 - antall ulike (sortert)
@@ -75,10 +75,10 @@ public class Oblig1 {
         int antall = 0;
 
         if (a.length > 0) {
-            //Dersom arrayet ikke er tom starter antall med 1 pga første tallet
+            //Dersom arrayet ikke er tomt starter antall med 1 pga det foerste tallet
             antall++;
         } else {
-            //Arrayet er tomt og retuneres 0
+            //Arrayet er tomt og vi returnerer 0
             return antall;
         }
 
@@ -110,59 +110,6 @@ public class Oblig1 {
         return antall;
     }
 
-    //TODO: Velge versjon og fjerne den annen
-
-    //Oppgave 4 - Delsortering
-    /*public static void delsortering(int[] a){
-        int temp;
-
-        for(int i = 0; i < a.length; i++) {
-            //Sjekk om tallet er et partall
-            if (a[i] % 2 == 0) {
-                for(int j = i+1; j < a.length; j++){
-                    //Finn et oddetall for å bytte plass med partallet
-                    if (a[j] % 2 != 0) {
-                        temp = a[i];
-                        a[i] = a[j];
-                        a[j] = temp;
-                        break;
-                    }
-                }
-            }
-        }
-
-        for(int i=0; i<a.length;i++) {
-            if (a[i] % 2 != 0) {
-                for(int j = 0; j < a.length; j++){
-                    if (a[j] % 2 != 0 && a[j] != a[i] && a[j] > a[i]){
-                        temp = a[i];
-                        a[i] = a[j];
-                        a[j] = temp;
-                        i--;
-                        if(i < 0){
-                            i = 0;
-                        }
-                        break;
-                    }
-                }
-            }
-            if (a[i] % 2 == 0) {
-                for(int j = 0; j < a.length; j++){
-                    if (a[j] % 2 == 0 && a[j] != a[i] && a[j] > a[i]){
-                        temp = a[i];
-                        a[i] = a[j];
-                        a[j] = temp;
-                        i--;
-                        if(i < 0){
-                            i = 0;
-                        }
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    */
     public static void delsortering(int[] a) {
         int j = a.length - 1;
 
@@ -229,10 +176,10 @@ public class Oblig1 {
     //Oppgave 6 - Rotere flere plasser
     public static void rotasjonFlerePlasser(char[] a, int k) {
         if (k < 0) {
-            // Omgjør tallet fra positivt til negativt og tar modulo på arrayet sitt lengde.
-            // Dette for å finne forskjellen på lengden til arrayet og tallet. Så vi vet hvor langt arrayet skal roteres til venstre.
-            // Vi gjør da så lengden på den første for-løkken blir så lang som vi vil rotere arrayet. Hvis lengden på arrayet er f.eks 10
-            // vil en k verdi med -4 bli til 6, så i stedet for å rotere 4 ganger til venstre roterer vi heller 6 ganger til høyre.
+            // Omgjoer tallet fra positivt til negativt og tar modulo på arrayet sitt lengde.
+            // Dette for aa finne forskjellen på lengden til arrayet og tallet. Så vi vet hvor langt arrayet skal roteres til venstre.
+            // Vi gjoer da saa lengden på den foerste for-loekken blir så lang som vi vil rotere arrayet. Hvis lengden på arrayet er f.eks 10
+            // vil en k verdi med -4 bli til 6, saa i stedet for å rotere 4 ganger til venstre roterer vi heller 6 ganger til hoeyre.
             k = -k % a.length;
             k = a.length - k;
         }
@@ -285,7 +232,7 @@ public class Oblig1 {
     public static int[] indekssortering(int[] a) {
         int[] temp = new int[a.length];
 
-        //Hjelpe tabell
+        //Hjelpetabell
         System.arraycopy(a, 0, temp, 0, a.length);
 
         int[] indeks = new int[a.length];
@@ -313,7 +260,7 @@ public class Oblig1 {
 
     //Oppgave 9 - Tredje minste tall
     public static int[] tredjeMin(int[] a) {
-        if (a.length < 3) throw new NoSuchElementException("Tabellet har mindre enn 3 verdier");
+        if (a.length < 3) throw new NoSuchElementException("Tabellen har mindre enn 3 verdier");
 
         int forstMin = a[0];
         int andreMin = a[1];
@@ -356,8 +303,6 @@ public class Oblig1 {
 
         return new int[]{forstMin, andreMin, tredjeMin};
     }
-
-    //TODO: Velge versjon og fjerne den annen
 
     //Oppgave 10 - Inneholdt
     public static boolean inneholdt(String a, String b) {
@@ -402,56 +347,5 @@ public class Oblig1 {
         }
 
         return inneholdt;
-
-
-        /*
-        for(int i = 0; i < a.length(); i++){
-            int countA = aMap.get(a.charAt(i));
-            int countB = bMap.get(a.charAt(i));
-
-            if(countA <= countB){
-                inneholdt = true;
-            } else {
-                return false;
-            }
-        }
-        return inneholdt;
-
-         */
-
-        /*
-            Set<Character> aSet = new HashSet<>();
-        Set<Character> bSet = new HashSet<>();
-        StringBuilder aSet2 = new StringBuilder();
-        StringBuilder bSet2 = new StringBuilder();
-
-        for(int i = 0; i <a.length(); i++) {
-            aSet.add(a.charAt(i));
-        }
-        for(int i = 0; i < b.length(); i++) {
-            bSet.add(b.charAt(i));
-        }
-
-        for(int i = 0; i < b.length(); i++) {
-            if(aSet.contains(b.charAt(i))) {
-                aSet2.append(b.charAt(i));
-            }
-        }
-        for(int i = 0; i < a.length(); i++) {
-            if(bSet.contains(a.charAt(i))) {
-                bSet2.append(a.charAt(i));
-            }
-        }
-
-        System.out.println(bSet2 + " " + aSet2);
-        if(!bSet.containsAll(aSet) || aSet2.length() < bSet2.length()) {
-            System.out.print("false");
-            return false;
-        } else {
-            System.out.print("true");
-            return true;
-        }
-
-         */
     }
 }
